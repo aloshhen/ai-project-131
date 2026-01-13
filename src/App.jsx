7552c7c123adc1259fdf69d8773d20f75d7ae4f7
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-13 19:41:25 +0100

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+import Features from './components/Features'
+
+function App() {
+  return (
+    <div className="bg-[#0F172A] text-white">
+      <Navigation />
+      <Hero />
+      <Features />
+    </div>
+  )
+}
+
+export default App
